d4964348f926a996dd409caecfa3f9f9c58f8f24
Zarcam <zarcam.t@gmail.com>
2024-02-02 16:59:40 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Lista de tareas KanBan",
+    'summary': "Lista Tareas Kanban",
+    'description': """
+        Lista de tareas formato KanBan
+    """,
+    'author': "ARG",
+    'version': '1.0',
+    'application': True,
+
+    'depends': ['project'],
+    'data': [
+            
+    ]
+}
